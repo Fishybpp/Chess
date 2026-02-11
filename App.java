1957446531c1fc59b86dea14c9ce1979be1784ec
Eli Yawson <fishybppgaming@gmail.com>
2026-02-11 16:39:27 -0500

    aa

--- App.java
+++ App.java
@@ -1,30 +1,11 @@
 package Programming.SER120.Chess;
 
-import Programming.SER120.Chess.game.GameManager;
+import Programming.SER120.Chess.board
 
 public class App {
 
-    public void scream(){
-        //
-    }
-
     public static void main(String[] args) {
         System.out.println("--- Starting SER120 Chess App ---");
-        
-        //App goofy = new App();
-        //App goober = new App();
-        //goofy.scream();
-        //goober.scream();
-
-        //App.scream();
-
-
-
-
-        //goober.scream();
-        GameManager gm = new GameManager();
-        gm.runGame();
-        
         System.out.println("--- Session Ended ---");
     }
 }
